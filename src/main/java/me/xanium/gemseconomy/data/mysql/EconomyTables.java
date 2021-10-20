7a33240ebf5e6d8f59fc09c34a912ce8d15a65be
justwei <admin@taylorswiftcn.com>
2021-10-20 20:41:59 +0800

    [fix] 字段长度不够

--- src/main/java/me/xanium/gemseconomy/data/mysql/EconomyTables.java
+++ src/main/java/me/xanium/gemseconomy/data/mysql/EconomyTables.java
@@ -6,7 +6,7 @@ public enum EconomyTables {
             new String[] {
                     "`uuid` varchar(36) NOT NULL",
                     "`player` varchar(48) NOT NULL",
-                    "`currency` varchar(32) NOT NULL",
+                    "`currency` varchar(36) NOT NULL",
                     "`balance` decimal(19,10) default 0",
                     "UNIQUE KEY `account` (`uuid`, `currency`)"
             }
